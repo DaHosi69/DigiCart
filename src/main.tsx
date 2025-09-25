2825dab92320a9c84f5a836aa1e8fb1b9b705596
DaHosi <hosnernoah5@gmail.com>
2025-09-25 17:15:44 +0200

    asdf

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './login/Login.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
